b10ad7ff0b71f1c952b9f62e74f0adbdced3be36
Cameron Wilson <caw522@uregina.ca>
2021-11-09 12:20:25 -0600

    add more directories and files

---
+++ ejs-eg/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+// this is a canonical alias to make your life easier, like jQuery to $.
+const app = express();
+// host static resources
+app.use(express.static("Public"));
+// body-parser is now built into express!
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+// a common localhost test port
+const port = 3000;
+
+// Simple server operation
+app.listen(port, () => {
+  // template literal
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.post("/logActivity", (req, res) => {
+  console.log("pain");
+  res.render("logActivity", {});
+});
